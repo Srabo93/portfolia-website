139f450d2dab1ca0ba80ffea89322f4870aad58e
srabo93 <a.s-dev@web.de>
2023-07-02 19:05:28 +0200

    removed Navigation
    going for one pager design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import ContactButton from "./components/ContactButton";
 import Hero from "./components/Hero";
-import Navigation from "./components/Navigation";
 import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <main>
-      <Navigation />
       <Hero />
       <Portfolio />
       <ContactButton />
