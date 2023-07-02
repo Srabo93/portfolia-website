6b0860be8da9774a3105ebf3df6d677a91597161
srabo93 <a.s-dev@web.de>
2023-07-02 17:13:05 +0200

    finished contact buttons
    added links and finegrained animations

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -10,22 +10,36 @@ const ContactButton = () => {
     <section className="flex flex-col fixed bottom-0 right-0 p-4">
       {show && (
         <>
-          <motion.button
-            className="btn btn-info btn-circle m-1 flex-1"
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.3 }}
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            href="https://www.linkedin.com/in/arminsrabovic/"
+            target="_blank"
           >
-            <img src={LinkedInSVG} alt="Contact me on LinkedIn" />
-          </motion.button>
-          <motion.button
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.2 }}
-            className="btn btn-info btn-circle m-1 flex-1"
+            <motion.button
+              className="btn btn-info btn-circle m-1 flex-1"
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.3 }}
+            >
+              <img src={LinkedInSVG} alt="Contact me on LinkedIn" />
+            </motion.button>
+          </motion.a>
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            href="https://github.com/Srabo93"
+            target="_blank"
           >
-            <img src={GithubSVG} alt="Contact me on Github" />
-          </motion.button>
+            <motion.button
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.2 }}
+              className="btn btn-info btn-circle m-1 flex-1"
+            >
+              <img src={GithubSVG} alt="Contact me on Github" />
+            </motion.button>
+          </motion.a>
         </>
       )}
       <motion.button
