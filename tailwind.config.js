68d933050f88e44d9d6790a52c41214b626d72f2
srabo93 <a.s-dev@web.de>
2023-07-02 16:37:02 +0200

    added custom Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,30 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f5e0dc",
+
+          secondary: "#89dceb",
+
+          accent: "#94e2d5",
+
+          neutral: "#cdd6f4",
+
+          "base-100": "#1e1e2e",
+
+          info: "#cba6f7",
+
+          success: "#a6e3a1",
+
+          warning: "#f9e2af",
+
+          error: "#f38ba8",
+        },
+      },
+    ],
+  },
 };
