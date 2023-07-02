9a72f6613436bbc387da350154bd1a7027b81699
srabo93 <a.s-dev@web.de>
2023-07-02 22:55:05 +0200

    added animation to portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,13 @@
+import { motion } from "framer-motion";
+
 const Projects = () => {
   return (
-    <section className="flex flex-wrap items-center p-5">
+    <motion.section
+      className="flex flex-wrap items-center p-5"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1 }}
+    >
       <article className="pw-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-4">
         <h3 className="text-3xl font-bold pb-3 text-secondary">Project Name</h3>
         <p className="text-neutral">
@@ -33,7 +40,7 @@ const Projects = () => {
           id nisi.
         </p>
       </article>
-    </section>
+    </motion.section>
   );
 };
 export default Projects;
