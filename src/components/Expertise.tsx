75846dc3fa2f5f0a2302f42895058357e09529a6
srabo93 <a.s-dev@web.de>
2023-07-05 16:14:42 +0200

    finished expertise

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -10,42 +10,42 @@ const Expertise = () => {
           Expertise
         </h2>
       </div>
-      <section className="grid grid-cols-3 gap-6 mt-10 mx-8 my-8 border-2 border-secondary">
-        <article className="py-5 border-e-2 border-secondary">
-          <div className="flex justify-center items-center pb-4">
-            <img src={ComputerSVG} className="w-12 mr-6" />
-            <h3 className="font-bold text-2xl text-secondary underline decoration-error underline-offset-8 decoration-8">
+      <section className="grid sm:grid-cols-3 md:grid-rows-3 md:grid-rows-none mt-10 my-4 mx-8 md:border-2 md:border-secondary">
+        <article className="flex flex-wrap justify-center content-start p-5 md:border-e-2 md:border-secondary">
+          <div className="flex sm:pb-2 md:pb-2 lg:pb-5">
+            <img src={ComputerSVG} className="flex-initial w-12 mr-2" />
+            <h3 className="flex-1 font-bold text-2xl text-secondary underline decoration-error underline-offset-8 decoration-8">
               Development
             </h3>
           </div>
-          <p className="text-neutral text-center">
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-            cillum sint consectetur cupidatat.
+          <p className="text-neutral text-center md:text-left p-2">
+            Experienced in Javascript/Typescript and PHP. Cloud Platforms of
+            Choice: Firebase and AWS
           </p>
         </article>
-        <article className="py-5 border-e-2 border-secondary">
-          <div className="flex justify-center items-center pb-4">
-            <img src={ReactSVG} className="w-12 mr-6" />
-            <h3 className="font-bold text-2xl text-secondary underline decoration-info underline-offset-8 decoration-8">
+        <article className="flex flex-wrap justify-center content-start p-5 md:border-e-2 md:border-secondary">
+          <div className="flex sm:pb-2 md:pb-2 lg:pb-5">
+            <img src={ReactSVG} className="flex-initial w-12 mr-2" />
+            <h3 className="flex-1 font-bold text-2xl text-secondary underline decoration-info underline-offset-8 decoration-8">
               Frontend
             </h3>
           </div>
-          <p className="text-neutral text-center">
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-            cillum sint consectetur cupidatat.
+          <p className="text-neutral  text-center md:text-left p-2">
+            HTML, CSS, TailwindCSS, MUI, JS, Redux, React and NextJS frameworks
           </p>
         </article>
-        <article className="py-5">
-          <div className="flex justify-center items-center pb-4">
-            <img src={NodeSVG} className="w-12 mr-6" />
-            <h3 className="font-bold text-2xl text-secondary underline decoration-warning underline-offset-8 decoration-8">
+        <article className="flex flex-wrap justify-center content-start p-5">
+          <div className="flex sm:pb-2 md:pb-2 lg:pb-5">
+            <img src={NodeSVG} className="flex-initial w-12 mr-2" />
+            <h3 className="flex-1 font-bold text-2xl text-secondary underline decoration-warning underline-offset-8 decoration-8">
               Backend
             </h3>
           </div>
-          <p className="text-neutral text-center">
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-            cillum sint consectetur cupidatat.
-          </p>
+          <ul className="text-neutral text-center md:text-left  p-2">
+            <li>Languages: NodeJS, PHP</li>
+            <li>Frameworks: ExpressJS, Symfony API-Platform</li>
+            <li>Databases: postgreSQL, MongoDB</li>
+          </ul>
         </article>
       </section>
     </>
